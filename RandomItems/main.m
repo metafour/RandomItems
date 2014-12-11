ce98dd223e455cfb9b7a45c7d7d54e3a90c569e3
Camron Schwoegler <camron@metafour.org>
2014-12-11 14:07:42 -0800

    Initial Commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Camron Schwoegler on 12/11/14.
+//  Copyright (c) 2014 metafour.org. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
